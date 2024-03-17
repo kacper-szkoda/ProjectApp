d2fca53754cf2bd35ac9f18bb695c2718a3bc13f
Kacper Szkoda <kacper.szkoda@student.kuleuven.be>
2024-03-17 19:49:40 +0100

    added slider gui, not clean yet

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+        JFrame test = new  JFrame();
+        test.add(new MySlider());
+        test.setVisible(true);
+    }
+}
